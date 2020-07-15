84dca8f1ac8df15bbd06f8ed8e4cb8aa25dc3960
goldenaem <yhc0253@naver.com>
2020-07-16 01:58:37 +0900

    Update models.py

--- models.py
+++ models.py
@@ -15,8 +15,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from utils import split_last, merge_last
+def split_last(x, shape):
+    "split the last dimension to given shape"
+    shape = list(shape)
+    assert shape.count(-1) <= 1
+    if -1 in shape:
+        shape[shape.index(-1)] = int(x.size(-1) / -np.prod(shape))
+    return x.view(*x.size()[:-1], *shape)
 
+def merge_last(x, n_dims):
+    "merge the last n_dims to a dimension"
+    s = x.size()
+    assert n_dims > 1 and n_dims < len(s)
+    return x.view(*s[:-n_dims], -1)
 
 class Config(NamedTuple):
     "Configuration for BERT model"
@@ -89,9 +100,9 @@ class MultiHeadedSelfAttention(nn.Module):
     """ Multi-Headed Dot Product Attention """
     def __init__(self, cfg):
         super().__init__()
-        self.proj_q = nn.Linear(cfg.hidden, cfg.hidden)
-        self.proj_k = nn.Linear(cfg.hidden, cfg.hidden)
-        self.proj_v = nn.Linear(cfg.hidden, cfg.hidden)
+        self.proj_q = nn.Linear(cfg.hidden, cfg.hidden) # Wq
+        self.proj_k = nn.Linear(cfg.hidden, cfg.hidden) # Wk
+        self.proj_v = nn.Linear(cfg.hidden, cfg.hidden) # Wv
         # self.drop = nn.Dropout(cfg.p_drop_attn)
         self.scores = None # for visualization
         self.n_heads = cfg.n_heads
@@ -102,12 +113,13 @@ class MultiHeadedSelfAttention(nn.Module):
         mask : (B(batch_size) x S(seq_len))
         * split D(dim) into (H(n_heads), W(width of head)) ; D = H * W
         """
+        # n_heads는 서로 다른 방법으로 attention을 학습하는 것들을 다시 모으는것.
         # (B, S, D) -proj-> (B, S, D) -split-> (B, S, H, W) -trans-> (B, H, S, W)
         q, k, v = self.proj_q(x), self.proj_k(x), self.proj_v(x)
         q, k, v = (split_last(x, (self.n_heads, -1)).transpose(1, 2)
                    for x in [q, k, v])
         # (B, H, S, W) @ (B, H, W, S) -> (B, H, S, S) -softmax-> (B, H, S, S)
-        scores = q @ k.transpose(-2, -1) / np.sqrt(k.size(-1))
+        scores = q @ k.transpose(-2, -1) / np.sqrt(k.size(-1)) # scale dot-product
         if mask is not None:
             mask = mask[:, None, None, :].float()
             scores -= 10000.0 * (1.0 - mask)
@@ -119,6 +131,9 @@ class MultiHeadedSelfAttention(nn.Module):
         h = merge_last(h, 2)
         self.scores = scores
         return h
+    
+    # h=6라 하고, n_head = 3이면, 6차원짜리 벡터가 seq 갯수만큼 존재. 이 때 Wq, Wk, Wv를 곱해서 q,k,v 가 각각 기존 x와 같은 size를 가지도록 함(B, s, 6)
+    # 이걸 이제 (B, s, 3, 2)로 쪼개고, merge 전까지 계산된 h는 (B, s, 3, 2)짜리 tensor인데  이를 (B, s, 6)짜리로 만듬. head마다의 결과를 concat함.
 
 
 class PositionWiseFeedForward(nn.Module):
@@ -163,9 +178,9 @@ class Transformer(nn.Module):
         # To used parameter-sharing strategies
         self.n_layers = cfg.n_layers
         self.attn = MultiHeadedSelfAttention(cfg)
-        self.proj = nn.Linear(cfg.hidden, cfg.hidden)
+        self.proj = nn.Linear(cfg.hidden, cfg.hidden) # head의 결과를 1번 proj한다음에 residual 연산을 수행.
         self.norm1 = LayerNorm(cfg)
-        self.pwff = PositionWiseFeedForward(cfg)
+        self.pwff = PositionWiseFeedForward(cfg) # 이제 seq마다 따로 FC를 연산을 수행해서 다시한번 마지막에 residual 연산을 수행.
         self.norm2 = LayerNorm(cfg)
         # self.drop = nn.Dropout(cfg.p_drop_hidden)
 
@@ -176,7 +191,7 @@ class Transformer(nn.Module):
             # h = block(h, mask)
             h = self.attn(h, mask)
             h = self.norm1(h + self.proj(h))
-            h = self.norm2(h + self.pwff(h))
+            h = self.norm2(h + self.pwff(h)) # [batch_size, seq_len, dimension]
 
         return h
 
